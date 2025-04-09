d70331d76b5ed9d5eb3ae13ba0112b64ec8455c3
PhucLe0809 <lhphuc.7.01j@gmail.com>
2025-04-09 09:42:20 +0700

    Fix server crashed

---
+++ src/main/java/com/mobile/api/model/redis/SessionRedis.java
@@ -0,0 +1,13 @@
+package com.mobile.api.model.redis;
+
+import jakarta.persistence.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@RedisHash("system_session_redis")
+public class SessionRedis {
+    @Id
+    private String id;
+    private String userId;
+    private Long createdAt;
+    private Long expiredAt;
+}
