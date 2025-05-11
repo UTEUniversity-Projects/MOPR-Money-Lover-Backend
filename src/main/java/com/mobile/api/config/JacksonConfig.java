049caf806af3ea314f51e97cb32b4cace44b03e4
LeTanTru <letantru@gmail.com>
2025-05-11 17:15:49 +0700

    fix create wallet

--- src/main/java/com/mobile/api/config/JacksonConfig.java
+++ src/main/java/com/mobile/api/config/JacksonConfig.java
@@ -1,7 +1,9 @@
 package com.mobile.api.config;
 
-import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.mobile.api.component.CustomDoubleSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,10 +13,15 @@ public class JacksonConfig {
     @Bean
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
-        SimpleModule module = new SimpleModule();
-        module.addSerializer(Double.class, new CustomDoubleSerializer());
-        module.addSerializer(Double.TYPE, new CustomDoubleSerializer());
-        mapper.registerModule(module);
+
+        SimpleModule doubleModule = new SimpleModule();
+        doubleModule.addSerializer(Double.class, new CustomDoubleSerializer());
+        doubleModule.addSerializer(Double.TYPE, new CustomDoubleSerializer());
+        mapper.registerModule(doubleModule);
+        // Register JavaTimeModule for Java date/time types
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         return mapper;
     }
 }
